be294ee317fa1ed85c82e15eafdbd35d83d91803
Sam Sepial <zprestoz@protonmail.com>
2022-02-08 18:22:04 -0500

    Created the layout of the page, contains the first/last name, email address and password/confirmation inputs

--- app/src/main/java/edu/temple/signupform/FormActivity.kt
+++ app/src/main/java/edu/temple/signupform/FormActivity.kt
@@ -2,8 +2,6 @@ package edu.temple.signupform
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.AdapterView
 import android.widget.Button
 import android.widget.TextView
 
@@ -12,9 +10,7 @@ class FormActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textview = findViewById<TextView>(R.id.getFirstName)
-        findViewById<Button>(R.id.button).setOnClickListener{
-            findViewById<TextView>(R.id.textView3).text = textview.text
-        }
+        val firstName = findViewById<TextView>(R.id.firstNameInput)
+        println(firstName)
     }
 }
