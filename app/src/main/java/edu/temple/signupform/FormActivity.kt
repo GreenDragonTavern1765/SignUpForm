1fdae193589283db4561915900c99d84b9084f6b
Sam Sepial <zprestoz@protonmail.com>
2022-02-08 13:04:40 -0500

    Created a basic form to get first name, and a button to print to screen. Need to make sure the value is correct

--- app/src/main/java/edu/temple/signupform/FormActivity.kt
+++ app/src/main/java/edu/temple/signupform/FormActivity.kt
@@ -2,10 +2,19 @@ package edu.temple.signupform
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Button
+import android.widget.TextView
 
 class FormActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textview = findViewById<TextView>(R.id.getFirstName)
+        findViewById<Button>(R.id.button).setOnClickListener{
+            findViewById<TextView>(R.id.textView3).text = textview.text
+        }
     }
 }
