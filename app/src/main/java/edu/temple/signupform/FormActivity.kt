1386f7151583c737ace0cbe473d0d8210595ad57
Sam Sepial <zprestoz@protonmail.com>
2022-02-09 12:59:37 -0500

    Two functions added. One will test if the password and confirmation values are the same, and the other will check if all the inputs are blank or not

--- app/src/main/java/edu/temple/signupform/FormActivity.kt
+++ app/src/main/java/edu/temple/signupform/FormActivity.kt
@@ -10,13 +10,32 @@ class FormActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val name = findViewById<TextView>(R.id.nameInput)
         val email = findViewById<TextView>(R.id.emailInput)
         val password = findViewById<TextView>(R.id.passwordInput)
         val confirm = findViewById<TextView>(R.id.confirmInput)
-        findViewById<Button>(R.id.createButton).setOnClickListener {
 
+        findViewById<Button>(R.id.createButton).setOnClickListener {
+            checkBlankInput(name.text.toString(), email.text.toString(), password.text.toString(), confirm.text.toString())
+            if (passwordMatch(password.text.toString(), confirm.text.toString()) == 0)
+                findViewById<TextView>(R.id.confirmInput).setError("Password do not match")
         }
+    }
+
+    fun passwordMatch(password: String, confirm: String) : Int {
+        if (password == confirm)
+            return 1
+        return 0
+    }
+
+    fun checkBlankInput(name: String, email: String, password: String, confirm: String) {
+        if (name.isBlank())
+            findViewById<TextView>(R.id.nameInput).setError("Please enter a valid name")
+        if (email.isBlank())
+            findViewById<TextView>(R.id.emailInput).setError("Please enter a valid email")
+        if (password.isBlank())
+            findViewById<TextView>(R.id.passwordInput).setError("Please enter a password")
+        if (confirm.isBlank())
+            findViewById<TextView>(R.id.confirmInput).setError("Please confirm the password")
     }
 }
