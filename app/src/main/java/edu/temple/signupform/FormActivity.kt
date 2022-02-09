f145513cb868437a1cbb95ee421b017582cd39ba
Sam Sepial <zprestoz@protonmail.com>
2022-02-09 11:16:40 -0500

    Changed the layout of the form to look more professional, and set values for each textView

--- app/src/main/java/edu/temple/signupform/FormActivity.kt
+++ app/src/main/java/edu/temple/signupform/FormActivity.kt
@@ -4,13 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import org.w3c.dom.Text
 
 class FormActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val firstName = findViewById<TextView>(R.id.firstNameInput)
-        println(firstName)
+        val name = findViewById<TextView>(R.id.nameInput)
+        val email = findViewById<TextView>(R.id.emailInput)
+        val password = findViewById<TextView>(R.id.passwordInput)
+        val confirm = findViewById<TextView>(R.id.confirmInput)
+        findViewById<Button>(R.id.createButton).setOnClickListener {
+
+        }
     }
 }
